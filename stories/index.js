9dd72d78fca5e602ac012b968ff4efd6aa5e8718
Ben Stolb <benstolb@protonmail.com>
2022-11-30 19:47:57 -0800

    Add Show component and stories

--- stories/index.js
+++ stories/index.js
@@ -13,6 +13,7 @@ import InterviewerList from "components/InterviewerList.js"
 import Appointment from "components/Appointment/index.js";
 import Header from "components/Appointment/Header.js";
 import Empty from "components/Appointment/Empty.js";
+import Show from "components/Appointment/Show"
 
 
 storiesOf("Button", module)
@@ -146,3 +147,11 @@ storiesOf("Appointment", module)
 .add("Appointment with Time", () => <Appointment time="12pm" />)
 .add("Header", () => <Header time="12pm" />)
 .add("Empty", () => <Empty onAdd={action("onAdd")}/>)
+.add("Show", () => (
+  <Show
+    student="Lydia Miller-Jones"
+    interviewer={interviewers[0]}
+    onEdit={action("onEdit")}
+    onDelete={action("onDelete")}
+  />
+))
