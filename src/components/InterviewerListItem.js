5a8bbd4c101bdfa15a33359d7b0cbb54e009614c
Ben Stolb <benstolb@protonmail.com>
2022-11-30 16:59:54 -0800

    refactor DayList and InterviewerList

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import classNames from 'classnames';
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  const { name, avatar, selected } = props;
+
+  const interviewerClass = classNames('interviewers__item', {
+    'interviewers__item--selected': selected,
+  });
+
+  return (
+    <li onClick={props.setInterviewer} className={interviewerClass}>
+      <img
+        className="interviewers__item-image"
+        src={avatar}
+        alt={name}
+      />
+      {selected && name}
+    </li>
+  );
+};
