15e34216da6fbd23bf16b10af820f5d726fa9dc2
Ben Stolb <benstolb@protonmail.com>
2022-12-04 14:25:56 -0800

    Fetch interviewer data and convert with helper function

--- src/components/Application.js
+++ src/components/Application.js
@@ -11,7 +11,8 @@ export default function Application(props) {
   const [state, setState] = useState({
     day: "Monday",
     days: [],
-    appointments: {}
+    appointments: {},
+    interviewers: []
   });
   
   const dailyAppointments = getAppointmentsForDay(state, state.day);
@@ -22,21 +23,26 @@ export default function Application(props) {
     Promise.all([
       axios.get('/api/days'),
       axios.get('/api/appointments'),
+      axios.get('/api/interviewers')
     ]).then((all) => {
-      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data}));
+      setState(prev => ({...prev, days: all[0].data, appointments: all[1].data, interviewers: all[2].data}));
     });
   }, []);
 
 
-  const appointmentArray= dailyAppointments.map(appointment => {
+  const schedule = dailyAppointments.map((appointment) => {
+    const interview = getInterview(state, appointment.interview);
+  
     return (
-      <Appointment 
-        key={appointment.id} 
-        {...appointment} 
+      <Appointment
+        key={appointment.id}
+        id={appointment.id}
+        time={appointment.time}
+        interview={interview}
       />
     );
   });
-
+    
   return (
     <main className="layout">
       <section className="sidebar">
@@ -60,7 +66,7 @@ export default function Application(props) {
       />
       </section>
       <section className="schedule">
-        {appointmentArray}
+        {schedule}
       </section>
     </main>
   );
--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -13,3 +13,14 @@ export function getAppointmentsForDay(state, day) {
 
   return daysAppointments;
 }
+
+export function getInterview(state, interview) {
+  
+  if(!interview) {
+    return null
+  }
+
+  const interviewer = state.interviewers[interview.interviewer];
+
+  return {...interview, interviewer}
+}
