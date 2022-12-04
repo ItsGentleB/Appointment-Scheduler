d5db7436c2b29a08a15e1c26fe9b108c22ad559a
Ben Stolb <benstolb@protonmail.com>
2022-12-04 12:46:31 -0800

    Add helper function and test to get app by day

---
+++ src/helpers/selectors.js
@@ -0,0 +1,15 @@
+export function getAppointmentsForDay(state, day) {
+  const filteredDay = state.days.filter(days => days.name === day);
+
+  if (filteredDay.length === 0) {
+    return filteredDay;
+  }
+
+  const filteredAppointments = filteredDay[0].appointments;
+
+  const daysAppointments = filteredAppointments.map(appointment => {
+    return state.appointments[appointment];
+  });
+
+  return daysAppointments;
+}
